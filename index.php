a62079c9904bf244c37c5a59c1b04aab2f89ae6b
Trent Schaller <trent.schaller@agapefrance.org>
2015-06-10 06:08:07 -0500

    updated quad placeholders

--- index.php
+++ index.php
@@ -47,31 +47,21 @@ defined('_JEXEC') or die;
     </nav>
 	<div class="container theme-showcase" role="main">
 		<jdoc:include type="modules" name="carousel" style="none" />
-		<div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-6">
-          <h2>Heading 1</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-6" style="padding: 15px;">
+          <img src="/images/quad/quad1.jpg" width="100%">
         </div>
-		<div class="col-md-6">
-          <h2>Heading 2</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		<div class="col-md-6" style="padding: 15px;">
+          <img src="/images/quad/quad2.jpg" width="100%">
         </div>
-        <div class="col-md-6">
-          <h2>Heading 3</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-6">
-          <h2>Heading 4</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		<div class="col-md-6" style="padding: 15px;">
+          <img src="/images/quad/quad3.jpg" width="100%">
+        </div>
+		<div class="col-md-6" style="padding: 15px;">
+          <img src="/images/quad/quad4.jpg" width="100%">
         </div>
       </div>
-    </div> <!-- /container -->
 		<!-- Main jumbotron for a primary marketing message or call to action -->
 		<div class="jumbotron">
 			<jdoc:include type="message" />
