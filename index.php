bb406349521e70ff8ab786410bc884c29fc3f6ab
Trent Schaller <trent.schaller@agapefrance.org>
2015-06-10 05:26:21 -0500

    moved carousel to module
    much easier for user to update and understand

--- index.php
+++ index.php
@@ -46,33 +46,7 @@ defined('_JEXEC') or die;
       </div>
     </nav>
 	<div class="container theme-showcase" role="main">
-
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-			</ol>
-			<div class="carousel-inner" role="listbox">
-				<div class="item active">
-					<jdoc:include type="modules" name="position-1" style="none" />
-				</div>
-				<div class="item">
-					<img data-src="holder.js/1140x500/auto/#666:#444/text:Second slide" alt="Second slide">
-				</div>
-				<div class="item">
-					<img data-src="holder.js/1140x500/auto/#555:#333/text:Third slide" alt="Third slide">
-				</div>
-			</div>
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
+		<jdoc:include type="modules" name="carousel" style="none" />
 		<!-- Main jumbotron for a primary marketing message or call to action -->
 		<div class="jumbotron">
 			<jdoc:include type="message" />
