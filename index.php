e14e713fc3653dc55616e566a70498b6a7e50e0a
David VIOLLEAU <david.violleau@agapefrance.org>
2015-07-07 09:45:06 +0200

    Chemins relatifs pour que ça marche si le site est dans un sous-répertoire
    Par exemple si le site n'est pas à la racine du domaine mais sous
    "/dev".

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ defined('_JEXEC') or die;
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 <link rel="icon" href="../../favicon.ico">
 <!-- Bootstrap core CSS -->
-<link href="/templates/jhct/css/bootstrap.min.css" rel="stylesheet">
+<link href="./templates/jhct/css/bootstrap.min.css" rel="stylesheet">
 <!-- Bootstrap theme -->
-<link href="/templates/jhct/css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="./templates/jhct/css/bootstrap-theme.min.css" rel="stylesheet">
 <!-- Custom styles for this template -->
-<link href="/templates/jhct/css/theme.css" rel="stylesheet">
+<link href="./templates/jhct/css/theme.css" rel="stylesheet">
 <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
 <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-<script src="/templates/jhct/js/ie-emulation-modes-warning.js"></script>
+<script src="./templates/jhct/js/ie-emulation-modes-warning.js"></script>
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -31,7 +31,7 @@ defined('_JEXEC') or die;
     <nav class="navbar navbar-default">
       <div class="container">
         <div class="navbar-header">
-		<a href="/"><img src="../templates/jhct/img/logo.png"/></a>
+		<a href="/"><img src="./templates/jhct/img/logo.png"/></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
@@ -68,9 +68,9 @@ defined('_JEXEC') or die;
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="/templates/jhct/js/bootstrap.min.js"></script>
-    <script src="/templates/jhct/js/docs.min.js"></script>
+    <script src="./templates/jhct/js/bootstrap.min.js"></script>
+    <script src="./templates/jhct/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/templates/jhct/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="./templates/jhct/js/ie10-viewport-bug-workaround.js"></script>
  </body>
 </html>
