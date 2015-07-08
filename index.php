a5e0e993c045af8b9152b0cf9d54f78bcf699eb4
David VIOLLEAU <david.violleau@agapefrance.org>
2015-07-08 15:59:50 +0200

    Responsive behaviour customisation

--- index.php
+++ index.php
@@ -16,75 +16,75 @@ defined('_JEXEC') or die;
 </head>
 <body data-spy="scroll" data-target=".bs-docs-sidebar">
 <?php /*include_once("analyticstracking.php")*/ ?>
-<!-- NAVBAR
-================================================== -->
-<div class="navbar-wrapper">
-	<!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
-	<div class="container">
-		<div class="logo navbar">
-			<div class="navbar-inner">
-				<a href="<?php echo $this->baseurl ?>">
-					<img src="./templates/jhctbootstrap23/img/fermedelafutaie_transp.png">
-					<h3>Ferme de la Futaie</h3>
-				</a>
+<!-- Wrap navbar and caroussel together to align the navbar at the bottom of the carousel -->
+<div class="header-wrapper">
+	<!-- NAVBAR
+	================================================== -->
+	<div class="navbar-wrapper">
+		<!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+		<div class="container">
+			<div class="logo navbar">
+				<div class="navbar-inner">
+					<a href="<?php echo $this->baseurl ?>">
+						<img src="./templates/jhctbootstrap23/img/fermedelafutaie_transp.png">
+						<h3>Ferme de la Futaie</h3>
+					</a>
+				</div>
+			</div>
+			<div class="navbar">
+				<div class="navbar-inner">
+					<!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="brand" href="<?php echo $this->baseurl ?>">Ferme de la Futaie</a>
+					<!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+					<div class="nav-collapse collapse">				
+						<jdoc:include type="modules" name="menu" style="none" />
+					</div><!--/.nav-collapse -->
+				</div><!-- /.navbar-inner -->
+			</div><!-- /.navbar -->
+		</div> <!-- /.container -->
+	</div><!-- /.navbar-wrapper -->
+	<!-- Carousel
+	================================================== -->
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-01.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-02.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-03.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-04.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-05.jpg" alt="" class="carouselimg">
+			</div>		
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-06.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-07.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-08.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-09.jpg" alt="" class="carouselimg">
+			</div>
+			<div class="car item">
+				<img src="./templates/jhctbootstrap23/img/slide-10.jpg" alt="" class="carouselimg">
 			</div>
 		</div>
-		<div class="navbar">
-			<div class="navbar-inner">
-				<!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="brand" href="<?php echo $this->baseurl ?>">Ferme de la Futaie</a>
-				<!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-				<div class="nav-collapse collapse">				
-					<jdoc:include type="modules" name="menu" style="none" />
-				</div><!--/.nav-collapse -->
-			</div><!-- /.navbar-inner -->
-		</div><!-- /.navbar -->
-	</div> <!-- /.container -->
-</div><!-- /.navbar-wrapper -->
-<!-- Carousel
-================================================== -->
-<div id="myCarousel" class="carousel slide">
-	<div class="carousel-inner">
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-01.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-02.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-03.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-04.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-06.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-07.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-08.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-09.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-10.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-11.jpg" alt="" class="carouselimg">
-		</div>
-		<div class="car item">
-			<img src="./templates/jhctbootstrap23/img/slide-12.jpg" alt="" class="carouselimg">
-		</div>
-	</div>
-</div><!-- /.carousel -->
+	</div><!-- /.carousel -->
+</div><!-- /.header-wrapper -->
 <!-- main container -->
 <div id="mainwrap">
 <div class='container'>
